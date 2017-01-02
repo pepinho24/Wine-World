6ab28c9092293614e0f4b1fc707b3de37dffca09
tsvetelina-aleksandrova <tsvety.alex@gmail.com>
2017-01-02 02:50:18 +0200

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,13 @@ import {Observable, Observer} from 'rxjs/Rx';
 import { ArticlesListComponent } from './components/articles-list/articles-list.component';
 import { routes } from './routes';
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
 
-//import { ArticlesServiceService } from './services/articles-service.service';
-//import { TruncatePipe } from './core/truncate.pipe';
 import { UsersService } from './services/users.service';
 import { AuthenticationService } from './services/authentication.service';
 
@@ -35,7 +34,8 @@ import { Authentication } from './services/authentication';
     RegisterComponent,
     LoginComponent,
     ArticleDetailComponent,
-    TruncatePipe
+    TruncatePipe,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
