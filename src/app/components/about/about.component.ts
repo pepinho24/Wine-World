eb225d8564fb636fdf4140b7c667c1e1f40538c5
tsvetelina-aleksandrova <tsvety.alex@gmail.com>
2017-01-02 21:10:14 +0200

    about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
+    email: string;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    subscribeToNewsletter() {
+        // TODO: validate email properly and send data to server
+        if (this.email && this.email.match(/.+@[a-z]+\.[a-z]+/)) {
+            window.alert(`Email ${this.email} has joined the party.\nThank you for subscribing to our wine newsletter!`);
+        }
+    }
 }
